1543b8834bc95e342879ed42ebf752a2d462c599
gdfederici <gdfederici@gmail.com>
2021-03-07 12:00:19 +0100

    Initial keyboard.

--- scripts/hangmangame.js
+++ scripts/hangmangame.js
@@ -1,2 +1,17 @@
 
 var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var testWord = "accavallavacca";
+
+
+function createKeyboard (letters) {
+    document.getElementById("provatemp").innerHTML = letters.map(function(item) {
+        return "<button onclick='playGame(\"" + item + "\")'>" + item + "</button>";
+    }).join(" ");
+}
+
+
+// *** ---------- TEST ---------- *** //
+createKeyboard (alphabet);
+function playGame(pippo) {
+    console.log(pippo);
+}
